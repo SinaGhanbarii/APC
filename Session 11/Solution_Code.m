f5fd73818ae0a3ca9f8fd28dfeff69d820435d26
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-06-03 20:12:31 +0200

    Update Solution_Code.m

--- Session 11/Solution_Code.m
+++ Session 11/Solution_Code.m
@@ -19,3 +19,29 @@ ktD0 = 3e8;     % [lit/mol/s]
 Cn = 25;        % [-]
 CRD = 180;      % [-]
 
+%% Resolution
+% Diffusion
+
+% No Diffusion
+%% Figures --- All Cases
+
+%% Function 1 --- Diffusion Case
+function dF = Batch_Diffusion(X,C)
+% Unknowns 
+I = C(1);
+M = C(2);
+% Calculations
+mM = M0.*(1-X).*MW_M.*V0;
+mP = mM0 - mM;          % [gr]
+wP = mP./(mP+mS0+mM);   % [-]
+% Kinetics Constants 
+kp = (1/kp0+exp(Cn.*wP)./kpD0).^(-1);   % [lit/mol/s]
+kt = ((1/kt0+exp(Cn.*wP)./ktD0.^(-1))+CRD*kp.*(1-wP));
+kfm = kp.*Cfm;  % [lit/mol/s]
+
+dI = -(kd.*I)./((kp+kfm).*(1-X).*sqrt(2.*f.*kd.*I./kt));
+dM = -M./(1-X);
+
+dF = [dI dM]';
+end
+%% Function 2 --- No Diffusion Case
